2494bd7ef64de658fd1312f68c06dfaa10a3fb27
secondwtq <lovejay-lovemusic@outlook.com>
2015-06-07 18:31:55 +0800

    Update.

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+/// <reference path="typings/node/node.d.ts"/>
+
+var path = require('path');
+
+var exus_env = {
+	
+};
 
 var express = require('express');
 var app = express();
@@ -10,11 +17,28 @@ var app = express();
 app.get('/hello', function (req, res) {
 	res.send('Hellor Worlder!'); });
 
-var server = app.listen(8000, function () {
+// var basicAuth = require('basic-auth-connect');
+// app.use(basicAuth('testUser', 'testPass'));
+
+var logger = require('morgan');
+var cookie_parser = require('cookie-parser');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hjs');
+
+app.use(logger('dev'));
+app.use(cookie_parser("I'm a secret."));
+
+var route_blog = require('./routes/blog');
+var route_user = require('./routes/user');
+app.use('/blog', route_blog);
+app.use('/user', route_user);
+
+app.use('/static', express.static('static'));
+
+var server = app.listen(process.env.PORT || 8000, function () {
 	var host = server.address().address;
 	var port = server.address().port;
 
 	console.log('Listening @ http://%s:%s', host, port);
 });
-
-app.use('/public', express.static('static'));
